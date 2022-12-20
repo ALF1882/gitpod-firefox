150eca53945417de9e01239918c8365a9cfdc248
ALF1882 <112660569+ALF1882@users.noreply.github.com>
2022-12-20 17:13:42 -0600

    Update firefox.bash

--- firefox.bash
+++ firefox.bash
@@ -1,1 +1,1 @@
-docker stop /firefox && docker rm /firefox && docker run -d --name=firefox -p 5800:5800 -v /docker/appdata/firefox:/config:rw --shm-size 2g jlesage/firefox
+docker run -d --name=firefox -p 5800:5800 -v /docker/appdata/firefox:/config:rw --shm-size 2g jlesage/firefox -rm
